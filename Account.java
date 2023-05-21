30f64af9d1a221a486718952f883ac307dfb7786
Cited6 <savanna.fifield@keyin.com>
2023-05-21 20:18:27 -0230

    Start

---
+++ Account.java
@@ -0,0 +1,23 @@
+public class Account{
+
+    private String id = "ABCD";
+    private String name = "Savanna";
+    private int balance = 0;
+
+
+    public Account(String id, String name)
+    {
+
+        this.id = "ABCD";
+        this.name = "Savanna";
+    }
+    public Account(String id, String name, int balance) 
+    {
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
+
+    }
+
+
+}
