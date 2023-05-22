78ca7587629b1509003ac6411f855cbb50519ab0
Cited6 <savanna.fifield@keyin.com>
2023-05-21 22:20:32 -0230

    committed

--- Account.java
+++ Account.java
@@ -1,22 +1,66 @@
 public class Account{
 
-    private String id = "ABCD";
-    private String name = "Savanna";
-    private int balance = 0;
+    private String id;
+    private String name;
+    private int balance;
 
 
     public Account(String id, String name)
     {
 
-        this.id = "ABCD";
-        this.name = "Savanna";
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
     }
     public Account(String id, String name, int balance) 
     {
         this.id = id;
         this.name = name;
-        this.balance = 0;
+        this.balance = balance;
 
+    }
+
+    public String getId() {
+        return id;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int credit(int amount) {
+        balance += amount;
+        return balance;
+    }
+
+    public int debit(int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else{
+            System.out.println("Insufficient funds");
+        }
+        return balance;
+    }
+
+    public int transferTo(Account another, int amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            another.credit(amount);
+
+        }else {
+            System.out.println("Insufficient funds");
+        }
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account [id=" + id + ", name=" + name + ", balance" + balance + "]";
     }
 
 
